252c0348e3482905dc03814e8c05a50add96cc58
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-04-24 16:23:46 +0530

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+    let slider = document.querySelector('.project-slider');
+    
+    
+    slider.addEventListener('click',()=>{
+    slider.classList.add('paused')
+    })
+*/
+// Example of listening to slide change event
